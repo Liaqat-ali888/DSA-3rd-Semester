b2f2df28f12aa73a29224725c7dd350b0e641b10
Liaqat Ali <liaqatalianjum1234@gmail.com>
2026-08-27 10:24:44 +0500

    Add files via upload

---
+++ Theory/class-1/code-extra-learning/list recursively/recursively-print.java
@@ -0,0 +1,40 @@
+class ll {
+    static void display(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+    }
+    static void displayr(Node head){
+        if(head ==null){
+            return;
+        }
+        System.out.print(head.data+" ");
+        displayr(head.next);
+    }
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }}
+    public static void main(String arg[]){
+        Node a = new Node(2);
+        Node b = new Node(3);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        Node e = new Node(6);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next =e;
+        display(a);
+        System.out.print("\n");
+
+        displayr(a);
+
+    }
+
+}
